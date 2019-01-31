1f8385fb24612a19c2cc198630d954d1c6c92deb
systemfailure <kefzce@gmail.com>
2019-01-31 22:32:54 +0200

    add project dependency skeleton && bring CI

---
+++ tests/DefaultTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class DefaultTest extends TestCase
+{
+    public function testMethod()
+    {
+        $this->assertTrue(true);
+    }
+}

11cccd252a9935294bcdcd9257fa15e18cba06ce
systemfailure <kefzce@gmail.com>
2019-02-06 01:16:37 +0200

    add few tests, clean up a bit

---
+++ tests/Http/ClientTest.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Http;
+
+use App\Http\ClientBuilder;
+use GuzzleHttp\ClientInterface;
+use PHPUnit\Framework\TestCase;
+
+class ClientTest extends TestCase
+{
+    /**
+     * @dataProvider OptionsProvider
+     *
+     * @param array $options
+     */
+    public function testCanCreateClient(array $options): void
+    {
+        $client = ClientBuilder::build($options);
+        $this->assertInstanceOf(ClientInterface::class, $client);
+        $this->assertEquals(
+            $client->getConfig(array_key_first($options)),
+            $options[array_key_first($options)]
+        );
+
+        $this->assertSame(
+            ClientBuilder::USERAGENT,
+            $client->getConfig('headers')['User-Agent'],
+        );
+    }
+
+    /**
+     * @return array
+     */
+    public function OptionsProvider(): array
+    {
+        return [
+            [['option' => 'value']],
+            [['anotherOption' => 'newValue']],
+            [['integer' => 123]],
+            [['class' => new \stdClass()]],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function IncorrectOptionProvider(): array
+    {
+        return [
+            ['option' => 'value'],
+            ['anotherOption' => 'newValue'],
+            ['integer' => 123],
+            ['class' => new \stdClass()],
+        ];
+    }
+
+    /**
+     * @dataProvider IncorrectOptionProvider
+     * @expectedException \TypeError
+     *
+     * @param $options
+     */
+    public function testShouldThrowExceptionIfIncorrectOptionsSet($options): void
+    {
+        $client = ClientBuilder::build($options);
+        dd($client);
+    }
+}

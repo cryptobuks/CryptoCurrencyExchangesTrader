9f71c38ee8b1679d837dd15c5dfe66b552bdca12
systemfailure <kefzce@gmail.com>
2019-02-24 03:45:49 +0200

    add Closure type hint

--- src/CryptoCurrencyExchanges/Command/ListProvidersCommand.php
+++ src/CryptoCurrencyExchanges/Command/ListProvidersCommand.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Kefzce\CryptoCurrencyExchanges\Command;
 
+use Kefzce\CryptoCurrencyExchanges\Provider\ProviderInterface;
 use Kefzce\CryptoCurrencyExchanges\Provider\ProviderResolver;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -38,14 +39,13 @@ final class ListProvidersCommand extends Command
      * @param \Symfony\Component\Console\Input\InputInterface   $input
      * @param \Symfony\Component\Console\Output\OutputInterface $output
      *
-     * @psalm-suppress MissingClosureParamType
-     *
      * @return int|void|null
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $io = new SymfonyStyle($input, $output);
 
+        /** @var ProviderInterface[] $providers */
         $providers = $this->resolver->getProviders();
         $io->success(
             sprintf(
@@ -55,7 +55,7 @@ final class ListProvidersCommand extends Command
         );
         $io->listing(
             array_map(
-                function ($value): string {
+                function (ProviderInterface $value): string {
                     return (new \ReflectionClass($value))->getShortName();
                 }, $providers)
         );
